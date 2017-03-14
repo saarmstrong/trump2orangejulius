68e9f666343a3d428c7f1954ca37ad668bbdb377
Andy Armstrong <andyarmstro@gmail.com>
2017-03-14 14:17:05 -0700

    regex tweak

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/trump/gi, 'Orange Julius');
+            var replacedText = text.replace(/(donald\s)?trump(\s{1})?/gi, 'Orange Julius ');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
